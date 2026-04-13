c66459f9856f9c22b9813486f8a19b8f9e06f0a2
rkhanna99 <59949597+rkhanna99@users.noreply.github.com>
2026-04-12 22:23:50 -0400

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        seen = set()
+        result_map = {}
+
+        # Edge case all numbers are positive
+        if nums[0] > 0:
+            return result
+
+        for i in range(0, len(nums) - 2):
+            # Check if we have already computed the values associated with our value at i
+            if nums[i] in seen:
+                continue
+
+            # Set the other pointers
+            j = i + 1
+            k = len(nums) - 1
+
+            # Solve for nums[j] + nums[k] = -nums[i]
+            target = -nums[i]
+
+            while j < k:
+                curr = nums[j] + nums[k]
+                if curr < target:
+                    j += 1
+                elif curr > target:
+                    k -= 1
+                else:
+                    potential_result = [nums[i], nums[j], nums[k]]
+
+                    key = frozenset(potential_result)
+
+                    if key not in result_map:
+                        result.append(potential_result)
+                        result_map[key] = 1
+
+                    j += 1
+                    k -= 1
+
+            seen.add(nums[i])
+
+        return result
+
+
+        
